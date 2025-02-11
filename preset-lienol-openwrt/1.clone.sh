ede02a235328193e1c3f28e4eafe086d9b858885
NiceOAO <52142674+NiceOAO@users.noreply.github.com>
2025-02-11 13:08:07 +0800

    Update 1.clone.sh

--- preset-lienol-openwrt/1.clone.sh
+++ preset-lienol-openwrt/1.clone.sh
@@ -16,6 +16,6 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
+# git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
 git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./ && rm -rf OpenClash
 git clone --depth 1 https://github.com/Lienol/openwrt-package.git && mv openwrt-package/* ./ && rm -rf openwrt-package
